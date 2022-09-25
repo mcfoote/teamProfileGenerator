09191d7ce82a79c5eaaefa173ded1467a5a60e5e
Mitchell Foote <mcfoote321@gmail.com>
2022-09-25 18:26:14 -0400

    initialize files and folders

---
+++ index.js
@@ -0,0 +1,35 @@
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getRole() {
+        return 'Employee';
+    }
+}
+
+class Manager extends Employee {
+
+}
+
+class Engineer extends Employee {
+
+}
+
+class Intern extends Employee {
+    
+}
