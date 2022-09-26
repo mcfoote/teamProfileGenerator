5dd9a8cc32542951807a459373831ebf4793e6fe
Mitchell Foote <mcfoote321@gmail.com>
2022-09-26 15:40:34 -0400

    more test code

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,13 +1,48 @@
-const Employee = require('../lib/Employee.s');
+const Employee = require('../lib/Employee.js');
 
-test('', () => {
-
+test('Able to create new Employee object', () => {
+    let testEmployee = new Employee();
+    expect(typeof(testEmployee)).toBe('object');
 });
 
-test('', () => {
-
+test('Test for name data member', () => {
+    const testName = 'test';
+    let testEmployee = new Employee(testName);
+    expect(testEmployee.name).toBe(testName);
 });
 
-test('', () => {
+test('Test for id data member', () => {
+    const testId = 1234;
+    let testEmployee = new Employee('test', testId);
+    expect(testEmployee.id).toBe(testId);
+});
 
+test('Test for email data member', () => {
+    const testEmail = 'testemail@gmail.com';
+    let testEmployee = new Employee('test', 1234, testEmail);
+    expect(testEmployee.email).toBe(testEmail);
+});
+
+test('getName returns name member', () => {
+    const testName = 'test';
+    let testEmployee = new Employee(testName);
+    expect(testEmployee.name).toBe(testName);
+});
+
+test('getId returns id member', () => {
+    const testId = 1234;
+    let testEmployee = new Employee('test', testId);
+    expect(testEmployee.id).toBe(testId);
+});
+
+test('getEmail returns email member', () => {
+    const testEmail = 'testemail@gmail.com';
+    let testEmployee = new Employee('test', 1234, testEmail);
+    expect(testEmployee.email).toBe(testEmail);
+});
+
+test('getRole returns Employee', () => {
+    const testVal = 'Employee';
+    let testEmployee = new Employee('test', 1234, 'testemail@gmail.com');
+    expect(testEmployee.getRole()).toBe(testVal);
 });
