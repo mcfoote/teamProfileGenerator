5dff4b656ca7ef96b114424db75de5f3f12760f8
Mitchell Foote <mcfoote321@gmail.com>
2022-09-26 15:49:14 -0400

    more test code

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -6,43 +6,43 @@ test('Able to create new Employee object', () => {
 });
 
 test('Test for name data member', () => {
-    const testName = 'test';
+    let testName = 'test';
     let testEmployee = new Employee(testName);
     expect(testEmployee.name).toBe(testName);
 });
 
 test('Test for id data member', () => {
-    const testId = 1234;
+    let testId = 1234;
     let testEmployee = new Employee('test', testId);
     expect(testEmployee.id).toBe(testId);
 });
 
 test('Test for email data member', () => {
-    const testEmail = 'testemail@gmail.com';
+    let testEmail = 'testemail@gmail.com';
     let testEmployee = new Employee('test', 1234, testEmail);
     expect(testEmployee.email).toBe(testEmail);
 });
 
 test('getName returns name member', () => {
-    const testName = 'test';
+    let testName = 'test';
     let testEmployee = new Employee(testName);
     expect(testEmployee.name).toBe(testName);
 });
 
 test('getId returns id member', () => {
-    const testId = 1234;
+    let testId = 1234;
     let testEmployee = new Employee('test', testId);
     expect(testEmployee.id).toBe(testId);
 });
 
 test('getEmail returns email member', () => {
-    const testEmail = 'testemail@gmail.com';
+    let testEmail = 'testemail@gmail.com';
     let testEmployee = new Employee('test', 1234, testEmail);
     expect(testEmployee.email).toBe(testEmail);
 });
 
 test('getRole returns Employee', () => {
-    const testVal = 'Employee';
+    let testVal = 'Employee';
     let testEmployee = new Employee('test', 1234, 'testemail@gmail.com');
     expect(testEmployee.getRole()).toBe(testVal);
 });
--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,13 +1,19 @@
 const Engineer = require('../lib/Engineer.js');
 
-test('', () => {
-
+test('Test for github data member', () => {
+    let testGithub = 'testgitname';
+    let testEngineer = new Engineer('test', 1234, 'testemail@gmail.com', testGithub);
+    expect(testEngineer.github).toBe(testGithub);
 });
 
-test('', () => {
-
+test('getGithub returns github member', () => {
+    let testGithub = 'testgitname';
+    let testEngineer = new Engineer('test', 1234, 'testemail@gmail.com', testGithub);
+    expect(testEngineer.getGithub()).toBe(testGithub);
 });
 
-test('', () => {
-
+test('getRole returns Engineer string', () => {
+    let testVal = 'Engineer';
+    let testEngineer = new Engineer('test', 1234, 'testemail@gmail.com', 'testgitname');
+    expect(testEngineer.getRole).toBe(testVal);
 });
