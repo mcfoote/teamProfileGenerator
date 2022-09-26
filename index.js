40a8abb1d4f2bafd9d19f72bb1066996bd9a2ff6
Mitchell Foote <mcfoote321@gmail.com>
2022-09-25 23:17:30 -0400

    base index code

---
+++ index.js
@@ -0,0 +1,29 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+
+const Manager = require('./lib/Manager.js');
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
+
+function main() {
+
+}
+
+function buildHTML() {
+
+}
+
+function addManager() {
+
+}
+
+function addEngineer() {
+
+}
+
+function addIntern() {
+    
+}
+
+main();
